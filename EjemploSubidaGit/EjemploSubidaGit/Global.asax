4ce955b8aa7d43c3c82943b4e3c6ba40e0ddcce0
duran221 <cristianfernandoduran@gmail.com>
2020-03-06 22:30:23 -0500

    subiendo el código

---
+++ EjemploSubidaGit/EjemploSubidaGit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EjemploSubidaGit.MvcApplication" Language="C#" %>
---
+++ EjemploSubidaGit/EjemploSubidaGit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EjemploSubidaGit.Startup))]
+namespace EjemploSubidaGit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
